20a966054d2b3ebc60d92eb3146e4ffc6c7a2c94
Melih Gultekin <mmelihgultekin@gmail.com>
2021-11-28 15:26:03 +0100

    Solve integer>fizzbuzz challenge

--- src/test/kotlin/com/igorwojda/integer/fizzbuzz/challenge.kt
+++ src/test/kotlin/com/igorwojda/integer/fizzbuzz/challenge.kt
@@ -3,8 +3,13 @@ package com.igorwojda.integer.fizzbuzz
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
-private fun fizzBuzz(n: Int): List<String> {
-    TODO("not implemented")
+private fun fizzBuzz(n: Int): List<String> = (1..n).toList().map { i ->
+    when {
+        (i.mod(15) == 0) -> "FizzBuzz"
+        (i.mod(3) == 0) -> "Fizz"
+        (i.mod(5) == 0) -> "Buzz"
+        else -> i.toString()
+    }
 }
 
 private class Test {
