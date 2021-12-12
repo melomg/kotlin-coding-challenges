0165b5c9f12f815af8f471f344abf8a74f500790
Melih Gultekin <mmelihgultekin@gmail.com>
2021-12-12 15:10:38 +0100

    Solve integer>stepsgenerator challenge

--- src/test/kotlin/com/igorwojda/integer/stepsgenerator/challenge.kt
+++ src/test/kotlin/com/igorwojda/integer/stepsgenerator/challenge.kt
@@ -3,8 +3,28 @@ package com.igorwojda.integer.stepsgenerator
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
+private const val HASH_SYMBOL: Char = '#'
+private const val SPACE: Char = ' '
+
 fun generateSteps(n: Int): List<String> {
-    TODO("not implemented")
+    if (n <= 0) return emptyList()
+
+    val result = mutableListOf<String>()
+
+    (0 until n).forEach { i ->
+        val symbolCount = i + 1
+        val spaceCount = (n - symbolCount)
+
+        var spaces = ""
+        var symbols = ""
+
+        repeat(spaceCount) { spaces += SPACE }
+        repeat(symbolCount) { symbols += HASH_SYMBOL }
+
+        result.add("$symbols$spaces")
+    }
+
+    return result
 }
 
 private class Test {
